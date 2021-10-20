f78a7c75de5b3333b82b28973b9c1eb2bf9c0073
chengzhibing1203@126.com <=>
2021-10-20 19:46:03 +0800

    增加设计模式

--- vueLearnNoWebpack/webpack20170925/app/Greeter.js
+++
@@ -1,8 +0,0 @@
-var config=require('./config.json');
-    //import styles from './Greeter.css';
-module.exports=function(){
-    var greeter=document.createElement('div');
-    //greeter.className=styles.root;
-    greeter.textContent=config.greetText;
-    return greeter;
-}
