78a97636011335e6b1c220ec76581a8c0e0f6c37
chengzhibing1203@126.com <=>
2022-01-03 09:54:07 +0800

    react提交

---
+++ studynew/reactdemo/src/ProjectTestApp.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+class ProjectTestApp extends Component{
+  render() {
+      return (
+          <div>
+              ProjectTestApp
+          </div>
+      )
+  }
+}
+export default ProjectTestApp
--- studynew/reactdemo/src/index.js
+++ studynew/reactdemo/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from "react-router-dom"
-import App from "./App"
-import "antd/dist/antd.css"
+// import {BrowserRouter} from "react-router-dom"
+// import App from "./App"
+// import "antd/dist/antd.css"
+
+import ProjectTestApp from "./ProjectTestApp"
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ProjectTestApp />,
   document.getElementById('root')
 );
 
